21636b7e6a6a82343b642c5dabfbbd6262486ddf
lihongfei <lihongfei1@xiaomi.com>
2018-09-22 12:25:52 +0800

    test revert defined file to history version

--- bin/copyAll.sh
+++ bin/copyAll.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
-Target=test/libthrift-0.11.0.so
+Target=libmds.so
 lib_array=`ldd $Target | grep -o "/.*" | grep -o "/.*/[^[:space:]]*"`
-LibDir=/home/mi/project/slicePro/sliceapi/bin/test
+LibDir=/root/workspace/bin
 for Variable in ${lib_array}
 do
 	    cp "$Variable" $LibDir
 done
+cp -r /usr/lib/x86_64-linux-gnu/qt5/plugins/imageformats /root/workspace/bin
+cp -r /usr/lib/x86_64-linux-gnu/qt5/plugins/platforms /root/workspace/bin
+cp $Target /root/workspace/bin
--- bin/test/change.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for file in /home/mi/project/slicePro/sliceapi/bin/test/*
-do
-echo $file
-patchelf --ser-rpath '$ORIGIN' $file
-done
